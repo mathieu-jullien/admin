761fe5aa2562d059057c4e61d3f67b4a72f415fc
Mathieu <hello@mathieujullien.fr>
2025-10-02 13:47:14 +0200

    Changement de texte

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -5,9 +5,10 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen">
       <div className="mb-6">
-        <h1 className="text-2xl font-bold text-gray-900">Dashboard</h1>
-        <p className="text-gray-600 mt-1">
-          Vue d'ensemble de votre portfolio et des interactions
+        <h1 className="text-2xl font-bold text-white">Dashboard</h1>
+        <p className="text-white mt-1">
+          Vue d'ensemble des statistiques portfolio et des interactions via le
+          formulaire de contact
         </p>
       </div>
 
